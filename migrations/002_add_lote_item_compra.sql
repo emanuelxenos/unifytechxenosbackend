91e5370de7c4b5d1bf327988b4edfbeb0dcad326
emanuelxenos <manuenbs@gmail.com>
2026-04-23 13:54:47 -0300

    cleanup: remover migrações consolidadas e arquivos temporários

--- migrations/002_add_lote_item_compra.sql
+++
@@ -1,6 +0,0 @@
--- Migração para adicionar campos de rastreabilidade na tabela item_compra
--- Esses campos permitem salvar o lote, localização e vencimento informados no lançamento da nota
-
-ALTER TABLE item_compra ADD COLUMN IF NOT EXISTS localizacao VARCHAR(100);
-ALTER TABLE item_compra ADD COLUMN IF NOT EXISTS data_vencimento DATE;
-ALTER TABLE item_compra ADD COLUMN IF NOT EXISTS lote VARCHAR(100);
